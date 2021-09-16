bb6abe05ec648150ed0e8892d89c2b2072a084b7
misja98 <mirko.djukic@icloud.com>
2021-09-16 21:04:37 +0200

    Update of sixth and push of seventh

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,14 +1,16 @@
 function main(numOne, numTwo) {
+
+   
     //Only change code below this line
 var numOne;
 var numTwo;
-var myQuotient = numOne / numTwo;
+var remainder = numOne % numTwo;
     //Only change code above this line
-    
-return myQuotient;
+
+return remainder;
 }
 
-console.log(main(5.0, 2.0)); 
-console.log(main(16.5, 5.5));
-console.log(main(102.0, 25.5));
+console.log(main(5, 2)); 
+console.log(main(15, 4));
+console.log(main(102, 25));
 module.exports = main;
