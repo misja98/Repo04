2244dc3b8c757d496b16fbfba2d465dc52081f02
misja98 <mirko.djukic@icloud.com>
2021-09-16 21:51:32 +0200

    10th example

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    //Only change code below this line
+var myFirstName = "Mirko";
+var myLastName = "Dukic";
+var myHomeTown = "Podgorica";
+    //Only change code above this line
+    
+return { 
+  myFirstName,
+  myLastName,
+  myHomeTown
+};
+}
+
+console.log(main()); 
+module.exports = main;
